3d5b27717db5f6c7ccb883e116fb519b955f3bf1
Woojin Kim <woojinwork16@gmail.com>
2025-04-10 06:54:16 +0900

    update code format

--- P2/xv6_ssu_mlfq/user/forktest.c
+++ P2/xv6_ssu_mlfq/user/forktest.c
@@ -5,52 +5,49 @@
 #include "stat.h"
 #include "user.h"
 
-#define N  1000
+#define N 1000
 
-void
-print(int fd, const char *s, ...)
+void print(int fd, const char *s, ...)
 {
-  write(fd, s, strlen(s));
+    write(fd, s, strlen(s));
 }
 
-void
-forktest(void)
+void forktest(void)
 {
-  int n, pid;
+    int n, pid;
 
-  print(1, "fork test\n");
+    print(1, "fork test\n");
 
-  for(n=0; n<N; n++){
-    pid = fork();
-    if(pid < 0)
-      break;
-    if(pid == 0)
-      exit();
-  }
-
-  if(n == N){
-    print(1, "fork claimed to work N times!\n", N);
-    exit();
-  }
-
-  for(; n > 0; n--){
-    if(wait() < 0){
-      print(1, "wait stopped early\n");
-      exit();
+    for (n = 0; n < N; n++) {
+        pid = fork();
+        if (pid < 0)
+            break;
+        if (pid == 0)
+            exit();
     }
-  }
 
-  if(wait() != -1){
-    print(1, "wait got too many\n");
-    exit();
-  }
+    if (n == N) {
+        print(1, "fork claimed to work N times!\n", N);
+        exit();
+    }
 
-  print(1, "fork test OK\n");
+    for (; n > 0; n--) {
+        if (wait() < 0) {
+            print(1, "wait stopped early\n");
+            exit();
+        }
+    }
+
+    if (wait() != -1) {
+        print(1, "wait got too many\n");
+        exit();
+    }
+
+    print(1, "fork test OK\n");
 }
 
-int
-main(void)
+int main(void)
 {
-  forktest();
-  exit();
+    forktest();
+    exit();
 }
