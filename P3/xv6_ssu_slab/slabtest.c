79f245e203dfbc90ac1ced0ea593c40119f621f1
Woojin Kim <woojinwork16@gmail.com>
2025-06-04 11:47:14 +0900

    Add P4 template(xv6 + test code)

--- P3/xv6_ssu_slab/slabtest.c
+++ P3/xv6_ssu_slab/slabtest.c
@@ -55,7 +55,7 @@ void slabtest()
     /* TIPS:
      *	You may debug your result with
      * cprintf();
-     * slabdump();
+     * // slabdump();
      */
 
     /* TEST1: Single slab alloc */
@@ -73,7 +73,7 @@ void slabtest()
 
 #ifdef SLAB_DEBUG
     end_time = rdtsc();
-    slabdump();
+    // slabdump();
     cprintf("%u\n", end_time - start_time);
 #endif
 
@@ -92,7 +92,7 @@ void slabtest()
 
 #ifdef SLAB_DEBUG
     end_time = rdtsc();
-    slabdump();
+    // slabdump();
     cprintf("%u\n", end_time - start_time);
 #endif
 
@@ -138,7 +138,7 @@ void slabtest()
     cprintf(pass ? "OK\n" : "WRONG\n");
 #ifdef SLAB_DEBUG
     end_time = rdtsc();
-    slabdump();
+    // slabdump();
     cprintf("%u\n", end_time - start_time);
 #endif
 
@@ -189,7 +189,7 @@ void slabtest()
     cprintf(pass ? "OK\n" : "WRONG\n");
 #ifdef SLAB_DEBUG
     end_time = rdtsc();
-    slabdump();
+    // slabdump();
     cprintf("%u\n", end_time - start_time);
 #endif
 
@@ -210,7 +210,7 @@ void slabtest()
     cprintf((!tmp && numobj_slab(TESTSLABID) == MAXTEST) ? "OK\n" : "WRONG\n");
 #ifdef SLAB_DEBUG
     end_time = rdtsc();
-    slabdump();
+    // slabdump();
     cprintf("%u\n", end_time - start_time);
 #endif
 
@@ -256,7 +256,7 @@ void slabtest()
     cprintf(pass ? "OK\n" : "WRONG\n");
 #ifdef SLAB_DEBUG
     end_time = rdtsc();
-    slabdump();
+    // slabdump();
     cprintf("%u\n", end_time - start_time);
 #endif
 
@@ -311,7 +311,7 @@ void slabtest()
     cprintf(pass ? "OK\n" : "WRONG\n");
 #ifdef SLAB_DEBUG
     end_time = rdtsc();
-    slabdump();
+    // slabdump();
     cprintf("%u\n", end_time - start_time);
 #endif
     /* TEST8: re-alloc empty slab pages */
@@ -361,7 +361,7 @@ void slabtest()
     cprintf(pass ? "OK\n" : "WRONG\n");
 #ifdef SLAB_DEBUG
     end_time = rdtsc();
-    slabdump();
+    // slabdump();
     cprintf("%u\n", end_time - start_time);
 #endif
 
@@ -405,7 +405,7 @@ void slabtest()
     cprintf(pass ? "OK\n" : "WRONG\n");
 #ifdef SLAB_DEBUG
     end_time = rdtsc();
-    slabdump();
+    // slabdump();
     cprintf("%u\n", end_time - start_time);
 #endif
     cprintf("===== TEST END =====\n");
---
+++ P4/xv6_ssu_sync/semaphore.h
@@ -0,0 +1,8 @@
+struct semaphore {
+
+    /* ******************** */
+    /* * WRITE YOUR CODE    */
+    /* ******************** */
+};
+
+extern struct semaphore usema[NLOCK];
